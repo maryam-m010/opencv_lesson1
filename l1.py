475c182ae18ab8e5a589fbb160f5d94561f357a5
maryam-m010 <mmaryam7@outlook.com>
2025-08-31 15:32:48 +0100

    Update l1.py

--- l1.py
+++ l1.py
@@ -18,3 +18,11 @@ cv2.destroyAllWindows()
 #cv2.IMREAD_COLOR (1) => Specify to load the image in color
 #cv2.IMREAD_GRAYSCALE (0) => Specify to load the image in grayscale / black & white
 #cv2.IMREAD_UNCHANGED (-1) => Specify to load the image unchanged
+path1 = r"C:\\Users\\mmary\\OneDrive - Queen Mary, University of London\\Maryam\\coding\\Jet Learn\\open cv\\pikachu.png"
+path2 = r"C:\\Users\\mmary\\OneDrive\\Desktop"
+
+import os
+print(os.listdir(path2)) #listdirectory
+os.chdir(path2) #change directory
+image = cv2.imread(path1)
+cv2.imwrite("pikachu.png", image)
